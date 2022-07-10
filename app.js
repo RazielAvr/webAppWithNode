a97ad249fa799e3ab8a4f5bbdb9033b26d755999
Raziel <razavraham2@gmail.com>
2022-07-10 17:07:15 +0300

    final-Templating Engines

--- app.js
+++ app.js
@@ -4,17 +4,22 @@ const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
 
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(morgan('tiny'));//get data from the page
 app.use(express.static(path.join(__dirname, '/public/')));//look into public folder and search index.html and run only this page
 
+
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
 app.get('/',(req, res)=>{
-    res.send('Hello from my app');
+    res.render('index', {title: 'Globomatics', data: ['a', 'b', 'c']});
 });//this run only if not found index.html file
 
-app.listen(3000, ()=>{
-    debug(`listening on port ${chalk.green('3000')}`);
+app.listen(PORT, ()=>{
+    debug(`listening to port ${chalk.green(PORT)}`);
 });
 
 
