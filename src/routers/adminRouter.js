b4d3ff467d54789ae38c20d378e6231142cc2dd7
Raziel <razavraham2@gmail.com>
2022-08-04 10:53:42 +0300

    problem with connect to DB-04.08.22

--- src/routers/adminRouter.js
+++ src/routers/adminRouter.js
@@ -8,16 +8,16 @@ let db = null;
 adminRouter.route('/').get((req, res) => {
    const uri = 'mongodb://Razielav:7X7J8LEDV253dk38@globomantics-shard-00-00.oa61v.mongodb.net:27017,globomantics-shard-00-01.oa61v.mongodb.net:27017,globomantics-shard-00-02.oa61v.mongodb.net:27017/globomantics?ssl=true&replicaSet=atlas-psofq6-shard-0&authSource=admin&retryWrites=true&w=majority';
    const url = `mongodb://localhost:27017`;
-   const uri2 = 'mongodb+srv://Razielav:7X7J8LEDV253dk38@globomantics.oa61v.mongodb.net/globomantics?retryWrites=true&w=majority';
+   const uri2 = 'mongodb+srv://Razielav:7X7J8LEDV253dk38@globomantics.oa61v.mongodb.net?retryWrites=true&w=majority';
    const config = {
     useNewUrlParser: true
   }
     const dbName = 'globomantics';
 
     (async function mongo(){
-        ;
+        
         try {
-            let client = await MongoClient.connect(uri,{ useNewUrlParser: true });
+            let client = await MongoClient.connect(uri2,{ useNewUrlParser: true });
             debug('Connected to the mongo DB');
            
             db = client.db();
