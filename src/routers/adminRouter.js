321b07f0cfbcefe309ef80ff231419734fcb6b52
Raziel <razavraham2@gmail.com>
2022-08-04 11:04:56 +0300

    problem with connect to DB-04.08.22-1

--- src/routers/adminRouter.js
+++ src/routers/adminRouter.js
@@ -4,7 +4,7 @@ const { MongoClient } = require('mongodb');
 const sessions = require('../data/sessions.json');
 
 const adminRouter = express.Router();
-let db = null;
+
 adminRouter.route('/').get((req, res) => {
    const uri = 'mongodb://Razielav:7X7J8LEDV253dk38@globomantics-shard-00-00.oa61v.mongodb.net:27017,globomantics-shard-00-01.oa61v.mongodb.net:27017,globomantics-shard-00-02.oa61v.mongodb.net:27017/globomantics?ssl=true&replicaSet=atlas-psofq6-shard-0&authSource=admin&retryWrites=true&w=majority';
    const url = `mongodb://localhost:27017`;
@@ -15,18 +15,20 @@ adminRouter.route('/').get((req, res) => {
     const dbName = 'globomantics';
 
     (async function mongo(){
-        
+        let client;
         try {
-            let client = await MongoClient.connect(uri2,{ useNewUrlParser: true });
-            debug('Connected to the mongo DB');
+            client = await MongoClient.connect(uri2);
+            //debug('Connected to the mongo DB');
+            console.log('Connected to the mongo DB');
            
-            db = client.db();
-            let collection = db.collection(`sessions`);
-            const response = await collection.insertMany(sessions);
+           const db = client.db(dbName);
+
+            const response = await db.collection('sessions').insertMany(sessions);
             res.json(response);
 
         } catch (error) {
-            debug(error.stack);
+           // debug(error.stack);
+           console.log(error.stack);
         }
          // Close connection
          //client.close();
