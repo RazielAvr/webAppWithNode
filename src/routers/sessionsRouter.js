3cf79c9a42863b99a90e198931dcc74179fb34aa
Raziel <razavraham2@gmail.com>
2022-08-08 14:39:08 +0300

    selecting Sessions-08.08.22-1

--- src/routers/sessionsRouter.js
+++ src/routers/sessionsRouter.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const sessionsRouter = express.Router();
 const debug = require('debug')('app:sessionsRouter');
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectID } = require('mongodb');
 const sessions = require('../data/sessions.json');
 
 
@@ -34,9 +34,34 @@ sessionsRouter.route('/').get((req, res) => {
 });
 sessionsRouter.route('/:id').get((req, res) => {
     const id = req.params.id;
-    res.render('session', {
-        session: sessions[id],
-    });
+    const uri2 = 'mongodb+srv://Razielav:7X7J8LEDV253dk38@globomantics.oa61v.mongodb.net?retryWrites=true&w=majority';
+    const config = {
+     useNewUrlParser: true
+   }
+     const dbName = 'globomantics';
+ 
+     (async function mongo(){
+         let client;
+         try {
+             client = await MongoClient.connect(uri2);
+             //debug('Connected to the mongo DB');
+             console.log('Connected to the mongo DB');
+            
+            const db = client.db(dbName);
+  
+             const sessions = await db.collection('sessions').findOne({_id: new ObjectID(id)});
+             res.render('session', {
+                sessions
+            });
+ 
+         } catch (error) {
+            //debug(error.stack);
+            console.log(error.stack);
+         }
+          // Close connection
+          client.close();
+     }());
+    
 });
 
 module.exports = sessionsRouter;
