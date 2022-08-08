1e8910963778f0fbc7fcc5afb06c760191f1d725
Raziel <razavraham2@gmail.com>
2022-08-08 16:55:11 +0300

    selecting Sessions-08.08.22-6

--- app.js
+++ app.js
@@ -3,17 +3,25 @@ const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
-
+const passport = require('passport');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 const sessionsRouter = require('./src/routers/sessionsRouter');
 const adminRouter = require('./src/routers/adminRouter');
+const authRouter = require('./src/routers/authRouter');
 
 app.use(morgan('tiny'));//get data from the page
 app.use(express.static(path.join(__dirname, '/public/')));//look into public folder and search index.html and run only this page
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+app.use(cookieParser());
+app.use(session({secret: 'globomantics'}));
 
+require('./src/config/passport.js')(app);
 
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
@@ -22,6 +30,7 @@ app.set('view engine', 'ejs');
 
 app.use('/sessions', sessionsRouter);
 app.use('/admin', adminRouter);
+app.use('/auth', authRouter );
 
 app.get('/',(req, res)=>{
     res.render('index', {title: 'Globomatics', data: ['a', 'b', 'c']});
---
+++ src/routers/authRouter.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const debug = require('debug')('app:authRouter');
+const { MongoClient, ObjectID } = require('mongodb');
+
+const authRouter = express.Router();
+
+authRouter.route('/signUp').post((req, res) =>{
+    const {username , password} = req.body;
+    const uri2 = 'mongodb+srv://Razielav:7X7J8LEDV253dk38@globomantics.oa61v.mongodb.net?retryWrites=true&w=majority';
+    const config = {
+     useNewUrlParser: true
+   }
+     const dbName = 'globomantics';
+
+     (async function addUser(){
+        let client;
+        try {
+            client = await MongoClient.connect(uri2);
+
+            const db = client.db(dbName);
+            const user = {username ,password};
+            const results = await db.collection('users').insertOne(user);
+            debug(results);
+            req.login(results.ops[0], () =>{
+                res.redirect('/auth/profile');
+            });
+        } catch (error) {
+            debug(error);
+        }
+        client.close();
+     }());
+});
+authRouter.route('/profile').get((req, res) =>{
+    res.json(req.user);
+});
+
+module.exports = authRouter;
