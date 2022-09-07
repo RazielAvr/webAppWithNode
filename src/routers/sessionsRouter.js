e911ba1c5b2fa4edabf2dc63ecfb15b57b251d9a
Raziel <razavraham2@gmail.com>
2022-09-07 16:13:23 +0300

    finish security and third-party APIs-07.09.22

--- src/routers/sessionsRouter.js
+++ src/routers/sessionsRouter.js
@@ -2,6 +2,7 @@ const express = require('express');
 const sessionsRouter = express.Router();
 const debug = require('debug')('app:sessionsRouter');
 const { MongoClient, ObjectID } = require('mongodb');
+const speakerService = require('../services/speakerService');
 
 const sessions = require('../data/sessions.json');
 
@@ -59,6 +60,9 @@ sessionsRouter.route('/:id').get((req, res) => {
             const db = client.db(dbName);
   
              const session = await db.collection('sessions').findOne({_id: new ObjectID(id)});
+             const speaker = await speakerService.getSpeakerById(session.speakers[0].id);
+
+             session.speaker = speaker.data;
              res.render('session', {
                 session
             });
